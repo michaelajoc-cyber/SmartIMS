253586ae04c24ae64ccc64accff6f832ceef7993
Michael Ajoc <zoezac@Michaels-MacBook-Air-2.local>
2026-05-11 20:19:50 +0700

    Fix staff service charge total

--- src/App.jsx
+++ src/App.jsx
@@ -1597,8 +1597,12 @@ Thank you.`
 
         <div class="totals-row grand-total">
           <span>Total</span>
-          <span>฿${doc.finalServiceCharge || 0}</span>
-        </div>
+          <span>฿${
+  doc.finalServiceCharge ||
+  (((doc.serviceAmount || doc.amount || 0) *
+    ((doc.staffShare || 100) / 100)) -
+    (doc.deductions || 0))
+}</span>
       `
       : `
         <div class="totals-row">
